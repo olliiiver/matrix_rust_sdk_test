a6032c6bd32a48a60169357776eee7e9979cf1da
olliiiver <olliiiver@example.com>
2024-12-04 12:59:54 +0100

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {  TextInput, Button } from 'react-native';
+import {
+    ClientBuilder,
+    ClientError,
+    SlidingSyncVersionBuilder,
+} from '@unomed/react-native-matrix-sdk';
+import RNFS from 'react-native-fs';
+import * as React from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [homeserver, setHomeserver] = React.useState('matrix.org');
+    const [status, setStatus] = React.useState('');
+
+    const updateHomeserverLoginDetails = React.useCallback(async () => {
+        if (!homeserver.length) {
+            setStatus('');
+            return;
+        }
+
+        try {
+            const tempDir = RNFS.TemporaryDirectoryPath;
+            console.log('Temp dir:' + tempDir);
+            const dataDir = RNFS.DocumentDirectoryPath + '/data';
+            const cacheDir = RNFS.DocumentDirectoryPath + '/cache';
+
+            const client = await new ClientBuilder()
+                .serverNameOrHomeserverUrl(homeserver)
+                .sessionPaths(dataDir, cacheDir)
+                .slidingSyncVersionBuilder(new SlidingSyncVersionBuilder.Native())
+                .build();
+            const loginDetails = await client.homeserverLoginDetails();
+
+            const sv = await client.availableSlidingSyncVersions();
+
+            setStatus('OK');
+
+            // await client.login('xx', 'xxx', 'foo', 'foo');
+            // setStatus('LOGGED IN');
+            // client.session();
+            // const syncService = await client.syncService().finish();
+            // await syncService.start();            
+
+        } catch (e) {
+            if (e instanceof ClientError.Generic) {
+                console.log(e.inner);
+                setStatus(e.inner.msg.toString());
+            } else {
+                console.log(e);
+                setStatus('Error');
+            }
+        }
+    }, [homeserver]);
+
+    return (
+        <View style={styles.container}>
+            <TextInput value={homeserver} onChangeText={setHomeserver}></TextInput>
+            <Button title="Go" onPress={updateHomeserverLoginDetails}></Button>
+            <Text>{status}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ README.md
@@ -0,0 +1,17 @@
+# Matrix rust SDK test
+
+````sh
+yarn install
+````
+
+# Build iOS (works)
+
+````sh
+npx expo run:ios
+````
+
+# Build Android (works not)
+
+````sh
+npx expo run:android
+````
